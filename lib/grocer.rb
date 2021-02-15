2159627128beb45e62517f1400dedcfc67344bfe
codified-likeness-utility <mr.daviesian@gmail.com>
2021-02-15 08:32:22 -0500

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,16 +1,17 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+require 'pry'
 
+def find_item_by_name_in_collection(name, collection)
+  collection.map do |key, value|
+    if key[:item] == name
+      return key
+    end
+  end
+  return nil
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+  find_item_by_name_in_collection[:item]
+  binding.pry
 end
 
 
